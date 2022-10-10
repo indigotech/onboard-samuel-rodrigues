fd4a7222f148cb972c05da39e4ee1aeaee0930f4
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-10-10 06:19:45 -0300

    Update typescript and apollo server

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { ApolloServer } from "apollo-server";
+import { resolvers } from "./graphql/resolvers";
+import { typeDefs } from "./graphql/schema";
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }: { url: String }) => {
+  console.log(`Server listening at ${url}`);
+});
