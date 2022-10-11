0530f23efe3ae238e4a7179a2f38d7fd33c9d3c9
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-10-11 09:10:35 -0300

    Configure TypeORM, PostgreSQL and Docker

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { ApolloServer } from 'apollo-server';
-import { resolvers } from './graphql/resolvers';
-import { typeDefs } from './graphql/schema';
+import 'reflect-metadata';
+import { AppDataSource } from './data-source';
+import { User } from './entity/User';
 
-const server = new ApolloServer({ typeDefs, resolvers });
+AppDataSource.initialize()
+  .then(async () => {
+    const user = new User();
+    user.firstName = 'Jonh';
+    user.lastName = 'Doe';
+    user.isActive = true;
+    await AppDataSource.manager.save(user);
 
-server.listen().then(({ url }: { url: string }) => {
-  console.log(`Server listening at ${url}`);
-});
+    const users = await AppDataSource.manager.find(User);
+    console.log(users);
+  })
+  .catch((error) => console.log(error));
