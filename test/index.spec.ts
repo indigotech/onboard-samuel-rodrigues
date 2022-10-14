232c960b10b2b154ae63b508d4347627751f792b
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-10-14 16:58:59 -0300

    Running the server for testing

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,12 +4,11 @@ import { AppDataSource } from '../src/data-source';
 import { resolvers } from '../src/graphql/resolvers';
 import { typeDefs } from '../src/graphql/schema';
 
-describe('Users query', function () {
-  before('[SERVER] Starting server', function (done) {
+describe('query hello', function () {
+  before('Starting server', function (done) {
     AppDataSource.initialize()
       .then(() => {
         const server = new ApolloServer({ typeDefs, resolvers });
-
         server.listen().then(({ url }) => {
           console.log(`Server listening at ${url}`);
         });
@@ -17,13 +16,10 @@ describe('Users query', function () {
       .catch((error) => console.log(error))
       .then(done);
   });
-  it('Returning the users:', async function () {
-    const endpoint = 'http://localhost:4000/';
-    const query = `query {
-      hello
-    }`;
+  it('Returning hello:', async function () {
+    const query = `query { hello }`;
 
-    const connection = axios.create({ baseURL: endpoint });
+    const connection = axios.create({ baseURL: 'http://localhost:4000/' });
     const result = await connection.post('/graphql', { query });
     console.table(JSON.stringify(result.data));
   });
