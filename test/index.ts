841e7d4bacfb33d09b70ab8702b18d2874c4fab4
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-10-14 05:16:23 -0300

    Add a simple test with mocha

---
+++ test/index.ts
@@ -0,0 +1,13 @@
+import { describe, it } from 'node:test';
+
+describe('test', () => {
+  it('test', () => {
+    console.log('Test ok');
+  });
+});
+
+describe('test2', () => {
+  it('test2', () => {
+    console.log('Test 2 ok');
+  });
+});
