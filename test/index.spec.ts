43b348bb148f03bdd8d364b79581ca499afdda58
SamuelHVRodrigues <samuelhvrodrigues@gmail.com>
2022-10-18 08:31:56 -0300

    Fix: Start the server once for testing

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,22 +1,12 @@
 import axios from 'axios';
-import { ApolloServer } from 'apollo-server';
-import { AppDataSource } from '../src/data-source';
 import { expect } from 'chai';
-import { resolvers } from '../src/graphql/resolvers';
-import { typeDefs } from '../src/graphql/schema';
+import { startServer } from '../src/index';
+
+before('Starting server', async () => {
+  await startServer;
+});
 
 describe('query hello', function () {
-  before('Starting server', function (done) {
-    AppDataSource.initialize()
-      .then(() => {
-        const server = new ApolloServer({ typeDefs, resolvers });
-        server.listen().then(({ url }) => {
-          console.log(`Server listening at ${url}`);
-        });
-      })
-      .catch((error) => console.log(error))
-      .then(done);
-  });
   it('Returning hello', async function () {
     const query = `query { hello }`;
 
